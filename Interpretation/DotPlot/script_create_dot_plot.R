6c7b8f6a02eef676fc80b05c965f4cf6d99ab373
urmovosa <urmo.vosa@gmail.com>
2017-05-15 17:31:38 +0200

    Fixed to add X and Y

--- Interpretation/DotPlot/script_create_dot_plot.R
+++ Interpretation/DotPlot/script_create_dot_plot.R
@@ -1,12 +1,8 @@
-#################################################################################################
-# This script takes the standard output of the eQTLMappingPipeline and constructs the dot-plot  #
-# SNP and probe positions have to be in hg19 coordinates                                        #
-# This script also needs the file chr_sizes, which was extracted from UCSC (GRCh37)             #
-# ###############################################################################################
-
-
 library(data.table)
 library(ggplot2)
+
+## Setup the graph
+# N = 13,913
 
 # Read in chromosome sizes
 chromosome <- fread('chr_sizes.txt')
@@ -24,7 +20,11 @@ for (i in 2:nrow(chromosome)){
   
 }
 
+xy$abs_coord <- NA
+chromosome <- rbind(chromosome, xy)
 
+chromosome$abs_coord[23] <- chromosome$abs_coord[22] + chromosome$size[23]
+chromosome$abs_coord[24] <- chromosome$abs_coord[23] + chromosome$size[24]
 
 # chrom tick marks
 
@@ -36,7 +36,7 @@ max_chr <- chromosome[chromosome$chr == 'Y', ]$abs_coord + chromosome[chromosome
 
 p <- ggplot(chromosome, aes(x = abs_coord, y = abs_coord)) + theme_bw() + 
   geom_vline(xintercept = c(chromosome$abs_coord, 0, max_chr), colour = 'lightgrey') + geom_hline(yintercept = c(chromosome$abs_coord, 0, max_chr), colour = 'lightgrey') + 
-  scale_x_continuous(breaks = chromosome$tick_mark, labels = paste("chr", chromosome$chr, sep = ''), limits = c(0, max(chromosome$abs_coord)), expand = c(0.01, 0.01)) + 
+  scale_x_continuous(breaks = chromosome$tick_mark, labels = paste("chr", chromosome$chr, sep = ''), limits = c(0, max(chromosome$abs_coord[1:22])), expand = c(0.01, 0.01)) + 
   scale_y_continuous(breaks = chromosome$tick_mark, labels = paste("chr", chromosome$chr, sep = ''), limits = c(0, max(chromosome$abs_coord)), expand = c(0.01, 0.01)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5), panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank()) + xlab('SNP position (hg19)') + ylab('Probe/gene center position (hg19)')
@@ -45,8 +45,8 @@ p <- ggplot(chromosome, aes(x = abs_coord, y = abs_coord)) + theme_bw() +
 # sort and tweak the data (trans or cis + trans eQTLsFDR0.05-Probelevel.txt)
 trans <- fread('eQTLsFDR0.05-ProbeLevel.txt')
 
-trans_f <- trans[, c(1, 2, 3, 4, 5, 6, 7, 11), with = F]
-
+#trans_f <- trans[, c(1, 2, 3, 4, 5, 6, 7, 11, 25:27), with = F]
+trans_f <- trans
 # Calculate absolute chromosome coordinates for SNPs and probes
 
 #
@@ -63,16 +63,25 @@ for (i in 2:22){
 #
 trans_f$abs_ProbeChrPos <- 0
 trans_f[trans_f$ProbeChr == 1, ]$abs_ProbeChrPos <- trans_f[trans_f$ProbeChr == 1, ]$ProbeCenterChrPos
+trans_f$abs_ProbeChrPos <- as.numeric(trans_f$abs_ProbeChrPos)
 
-for (i in 2:22){
+for (i in c(2:24)){
   
-  trans_f[trans_f$ProbeChr == i, ]$abs_ProbeChrPos <- trans_f[trans_f$ProbeChr == i, ]$ProbeCenterChrPos + chromosome[chromosome$chr == i - 1, ]$abs_coord
+  trans_f[trans_f$ProbeChr == chromosome$chr[i], ]$abs_ProbeChrPos <- as.numeric(trans_f[trans_f$ProbeChr == chromosome$chr[i], ]$ProbeCenterChrPos) + as.numeric(chromosome[chromosome$chr == chromosome$chr[i - 1], ]$abs_coord)
   
 }
 
 # visualize locations on dot-plot
 
+# Size is -log10(P-value)
 p + geom_point(data = as.data.frame(trans_f), aes(x = abs_SNPChrPos, y = abs_ProbeChrPos, size = -log10(PValue)), alpha = 0.15, colour = 'black') + 
-  scale_size_continuous(breaks = c(10, 50, 100, 200, 300), range = c(1, 5), guide = guide_legend(title = expression(paste(-log[10]("P-value")))))
+  scale_size_continuous(breaks = c(10, 50, 100, 200, 300), range = c(1, 5), guide = guide_legend(title = expression(paste(-log[10]("P-value"))))) + 
+  theme(axis.title=element_text(size = 18, face = "bold")) + ylab('Gene position (hg19)')
 
-ggsave('dot_plot_BIOS.png', height = 9, width = 9 * 1.2, dpi = 400)
+ggsave('dot_plot.png', height = 9, width = 9 * 1.1, dpi = 400)
+
+# Size is Z-scpre instead of P-value
+p + geom_point(data = as.data.frame(trans_f), aes(x = abs_SNPChrPos, y = abs_ProbeChrPos, size = abs(OverallZScore)), alpha = 0.15, colour = 'black') + 
+  scale_size_continuous( range = c(1, 5), guide = guide_legend(title = expression(paste("meta-analysis Z-score"))))
+
+ggsave('dot_plot_Z.png', height = 9, width = 9, dpi = 400)
