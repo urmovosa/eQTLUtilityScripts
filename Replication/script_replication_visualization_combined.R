f4ed7110949e1a13a19cee8b19f860686df79696
urmovosa <urmo.vosa@gmail.com>
2018-02-04 00:26:19 +0100

    Add combined plot

---
+++ Replication/script_replication_visualization_combined.R
@@ -0,0 +1,173 @@
+#############################################################################################################
+# This script uses discovery meta-analysis output, replication output files and constructs replication plot #
+#############################################################################################################
+
+library(data.table)
+library(stringr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(gridExtra)
+
+# file with data paths
+# columns:
+# Disc_path: full path to discovery dataset
+# Rep_path: full path to replication analysis daset	
+# Disc_name: discovery dataset name occurring on the plot	
+# Rep_name: replication dataset name occurring on the plot
+
+data_loc <- fread('path_file.txt')
+
+# do plots in loop
+
+pl <- list()
+
+for (i in c(1:nrow(data_loc))){
+
+## read in the discovery meta-analysis results:
+
+discovery <- fread(paste("gunzip -c", data_loc$Disc_path[i], sep = ' '))
+
+##  replication:
+
+replication <- fread(paste("gunzip -c", data_loc$Rep_path[i], sep = ' '))
+
+# calculate the sample sizes for discovery and replication
+
+
+Ndisc <- t(as.data.frame(str_split(unique(discovery$DatasetsNrSamples), ';')))
+#rownames(Ndisc) <- paste(discovery$SNPName, discovery$ProbeName)
+Ndisc[Ndisc == "-"] <- NA
+
+if (class(Ndisc) == 'matrix' & ncol(Ndisc) > 1){
+  Ndisc <- apply(Ndisc, 2, as.numeric)
+  Ndisc <- Ndisc[complete.cases(Ndisc), ]
+  if(class(Ndisc) == 'matrix'){
+    Ndisc <- max(rowSums(Ndisc))
+  } else (Ndisc <- sum(Ndisc))
+} else {Ndisc <- max(as.numeric(Ndisc))}
+
+
+#if(class(Ndisc) == 'vector' & length(Ndisc) < 1){stop("None of the discovery eQTLs is present in all of the cohorts!")}
+
+# if(class(Ndisc) == 'matrix' & ncol(Ndisc) > 1){
+# Ndisc <- max(rowSums(Ndisc))
+# } else {Ndisc <- unique(Ndisc)}
+
+colnames(replication)[c(2, 5, 14)] <- c('SNPName', 'ProbeName', 'DatasetsNrSamples')
+
+Nrep <- t(as.data.frame(str_split(unique(replication$DatasetsNrSamples), ';')))
+dim(Nrep)
+dim(replication)
+#rownames(Nrep) <- paste(replication$SNPName, replication$ProbeName)
+Nrep[Nrep == "-"] <- NA
+
+
+if (class(Nrep) == 'matrix' & ncol(Nrep) > 1){
+  Nrep <- apply(Nrep, 2, as.numeric)
+  Nrep <- Nrep[complete.cases(Nrep), ]
+  if(class(Nrep) == 'matrix'){
+    Nrep <- max(rowSums(Nrep))
+  } else (Nrep <- sum(Nrep))
+} else {Nrep <- max(as.numeric(Nrep))}
+
+print(Nrep)
+
+#if(class(Nrep) == 'vector' & length(Nrep) < 1){stop("None of the replication eQTLs is present in all of the cohorts!")}
+
+# if(class(Nrep) == 'matrix'){
+#   Nrep <- max(rowSums(Nrep))
+# } else {Nrep <- unique(Nrep)}
+
+# merge together 
+dim(discovery)
+discovery$pair <- paste(discovery$SNPName, discovery$ProbeName, sep = '_')
+dim(discovery)
+discovery <- discovery[, c(23, 10, 11), with = F]
+dim(discovery)
+colnames(discovery) <- paste0('Discovery_', colnames(discovery))
+replication$pair <- paste(replication$SNPName, replication$ProbeName, sep = '_')
+colnames(replication)[c(10, 11, 23)] <- c('AlleleAssessed', 'OverallZScore', 'pair')
+replication <- replication[, c(23, 10, 11, 22), with = F]
+colnames(replication) <- paste0('Replication_', colnames(replication))
+
+
+replication$sig_replication <- 'no'
+replication[replication$Replication_FDR < 0.05, ]$sig_replication <- 'yes'
+all_merged <- merge(discovery, replication, by.x = 'Discovery_pair', by.y = 'Replication_pair', all.x = T)
+all_merged[!all_merged$Discovery_AlleleAssessed == all_merged$Replication_AlleleAssessed, ]$Replication_OverallZScore <- -all_merged[!all_merged$Discovery_AlleleAssessed == all_merged$Replication_AlleleAssessed, ]$Replication_OverallZScore
+all_merged$same_dir <- 'no'
+all_merged[(all_merged$Discovery_OverallZScore > 0 & all_merged$Replication_OverallZScore > 0) |
+             (all_merged$Discovery_OverallZScore < 0 & all_merged$Replication_OverallZScore < 0) ]$same_dir <- 'yes'
+all_merged$replicated <- 'no'
+all_merged[all_merged$sig_replication == 'yes' & all_merged$same_dir == 'yes', ]$replicated <- 'yes'
+
+# calculate statistics:
+statistics_overview <- all_merged %>%
+  summarize(nr_of_disc_assoc = length(Replication_OverallZScore), 
+            nr_of_tested_in_replication = sum(!is.na(Replication_OverallZScore)),
+            same_direction = sum(same_dir == 'yes'),
+            nr_of_sig_in_replication = sum(!is.na(Replication_OverallZScore) & sig_replication == 'yes'),
+            nr_of_replicated_same_dir = sum(!is.na(Replication_OverallZScore) & sig_replication == 'yes' & same_dir == 'yes')) %>%
+  mutate(tested_eqtl_perc = round(nr_of_tested_in_replication/nr_of_disc_assoc, digits = 4) * 100,
+         same_dir_perc = round(same_direction/nr_of_tested_in_replication, digits = 4) * 100,
+         replication_perc = round(nr_of_replicated_same_dir/nr_of_tested_in_replication, digits = 4) * 100,
+         replicated_same_dir_perc = round(nr_of_replicated_same_dir/nr_of_sig_in_replication, digits = 4) * 100)
+
+# define limits for x and y axes
+x_axis <- max(abs(min(all_merged$Discovery_OverallZScore)), abs(max(all_merged$Discovery_OverallZScore, na.rm = T)), na.rm = T)
+x_axis <- x_axis + 0.1 * x_axis
+y_axis <- max(abs(min(all_merged$Replication_OverallZScore)), abs(max(all_merged$Replication_OverallZScore, na.rm = T)), na.rm = T)
+y_axis <- y_axis + 0.1 * y_axis
+
+ann_text <- data.frame(Discovery_OverallZScore = -x_axis, 
+                       Replication_OverallZScore = y_axis,
+                       lab = paste0("Nr. of discovery associations: \n", statistics_overview$nr_of_disc_assoc, 
+                                    "\nTested in replication cohort: \n", statistics_overview$nr_of_tested_in_replication, ' (', statistics_overview$tested_eqtl_perc, '%)',
+                                    "\nSig. replicated in replication cohort: \n", statistics_overview$nr_of_sig_in_replication, ' (', round(statistics_overview$nr_of_sig_in_replication/statistics_overview$nr_of_tested_in_replication * 100, 2), '%)',
+                                    "\nSig. replicated with same direction: \n", statistics_overview$nr_of_replicated_same_dir, ' (', round(statistics_overview$nr_of_replicated_same_dir/statistics_overview$nr_of_sig_in_replication * 100, 2), '%)'),
+                       `Replication\nFDR<0.05` = factor('yes', levels = c('yes', 'no')))
+
+colnames(ann_text)[ncol(ann_text)] <- "Sig. replication"
+
+###
+colnames(all_merged)[9] <- 'Sig. replication'
+colnames(all_merged)[7] <- 'FDR<0.05'
+all_merged <- all_merged[!is.na(all_merged$`FDR<0.05`), ]
+
+all_merged$`FDR<0.05` <- factor(all_merged$`FDR<0.05`, 
+                                levels = c('yes', 'no'))
+all_merged$`Effect in replication\ncohort:` <- 'non sig.'
+all_merged[all_merged$`FDR<0.05` == 'yes' & all_merged$same_dir == 'no', ]$`Effect in replication\ncohort:` <- 'FDR<0.05 and opp. dir.'
+all_merged[all_merged$`FDR<0.05` == 'yes' & all_merged$same_dir == 'yes', ]$`Effect in replication\ncohort:` <- 'FDR<0.05 and same dir.'
+all_merged$`Effect in replication\ncohort:` <- factor(all_merged$`Effect in replication\ncohort:`, levels = c('non sig.', 'FDR<0.05 and opp. dir.', 'FDR<0.05 and same dir.'))
+
+p <- ggplot(all_merged, aes(x = Discovery_OverallZScore, y = Replication_OverallZScore, colour = `Effect in replication\ncohort:`)) + 
+  geom_point(alpha = 0.35) + 
+  theme_classic() +
+  geom_hline(yintercept = 0, linetype = 2, size = 0.5) + 
+  geom_vline(xintercept = 0, linetype = 2, size = 0.5) + 
+  scale_color_manual(values = c("FDR<0.05 and same dir." = "red", "FDR<0.05 and opp. dir." = "orange", "non sig." = "darkgrey"), drop = FALSE) + 
+  xlab('Discovery analysis Z-score') + 
+  ylab('Replication analysis Z-score') +
+  geom_text(data = ann_text, aes(label = lab), size = 4, hjust=0, vjust = 1, col = 'black') +
+  ggtitle(paste0('Discovery cohort: ', data_loc$Disc_name[i], ' (N=', Ndisc, ')\nReplication cohort: ', data_loc$Rep_name[i], ' (N=', Nrep, ')')) +
+  scale_y_continuous(limits = c(-y_axis, y_axis)) + 
+  scale_x_continuous(limits = c(-x_axis, x_axis))
+
+pl[[i]] <- p
+print(data_loc[i, ])
+
+}
+
+# arrange plots
+
+hlay <- matrix(nrow = length(unique(data_loc$Disc_path)), 
+               ncol = length(unique(data_loc$Disc_path)), 0, byrow = TRUE)
+diag(hlay) <- NA
+hlay[!is.na(hlay)] <- 1:12
+hlay <- t(hlay)
+
+ml <- grid.arrange(grobs = pl, layout_matrix = hlay)
+
+ggsave(paste0('Combined', '_', Sys.Date(), '.pdf'), ml, height = 7 * 1.2 * 3.25, width = 8 * 1.2 * 3.25, dpi = 600, units = 'in')
